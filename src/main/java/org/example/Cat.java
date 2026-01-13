355843c1ee42f08429fc0fd8bccc27ce120e1fbd
Demkiv Maxym <demkivmaxym@gmail.com>
2026-01-13 10:41:40 +0200

    Add Animal, Cat and Dog

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Cat extends Animal {
+    private String breed;
+
+    public Cat(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+}
+
