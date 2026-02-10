88c3121262d99305bc965c196302a1282793cfa6
Demkiv Max <146538140+Maxdemm@users.noreply.github.com>
2026-02-10 10:39:25 +0200

    add abstract ReportGenerator and DailyReport impl

---
+++ src/main/java/org/example/pract5/ReportGenerator.java
@@ -0,0 +1,19 @@
+package org.example.pract5;
+
+public abstract class ReportGenerator {
+
+    public final void generate() {
+        loadData();
+        processData();
+        exportData();
+    }
+
+    abstract void exportData();
+    abstract void processData();
+
+    private void loadData() {
+        System.out.println("load data");
+    }
+
+
+}
