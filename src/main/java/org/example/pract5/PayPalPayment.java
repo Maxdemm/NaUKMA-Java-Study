12726060451cc6b176261f7509f413e82a3afb74
Demkiv Max <146538140+Maxdemm@users.noreply.github.com>
2026-02-10 10:37:01 +0200

    add PaymentMethod hierarchy

---
+++ src/main/java/org/example/pract5/PayPalPayment.java
@@ -0,0 +1,13 @@
+package org.example.pract5;
+
+public class PayPalPayment implements PaymentMethod {
+    @Override
+    public String name() {
+        return "PayPalPayment";
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println("paypal | сума: " + amount);
+    }
+}
