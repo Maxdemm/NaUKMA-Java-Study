5240cd14a68864c11b1fe3b436ee7dd8b301f0b6
Demkiv Maxym <demkivmaxym@gmail.com>
2026-04-21 12:06:06 +0300

    feat: add reflection practice

---
+++ src/main/java/org/example/pract15/task3/PracticeMainTask3.java
@@ -0,0 +1,9 @@
+package org.example.pract15.task3;
+
+
+public class PracticeMainTask3 {
+    public static void main(String[] args) {
+        SetupFlow flow = new SetupFlow();
+        StepRunner.run(flow);
+    }
+}
